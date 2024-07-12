3949d9455c3a5ba74f252b74716249de571e1681
ronak4195 <4195rj@gmail.com>
2024-07-12 13:41:52 +0530

    workUpdate

--- src/components/data/projectData.js
+++ src/components/data/projectData.js
@@ -1,32 +1,48 @@
 let projectData = [
     {
+        link: "https://github.com/ronak4195/docker-container",
+        title: "Linux Container",
+        date: "14/05/2024",
+        tech: ["C", "Docker", "Container", "Linux"],
+        desc:"Implemented a comprehensive Linux container solution focusing on minimal security restrictions for running untrusted code. The project explores namespaces, capabilities, cgroups, and setrlimit mechanisms to isolate kernel objects and limit resource usage. The literate code, formatted in noweb-style within an orgmode document, prioritizes understanding unsafe permissions and effective security practices."
+    },
+    {
         link: "https://github.com/ronak4195/portfolio",
         title: "Portfolio",
         date: "12/04/2024",
         tech: ["ReactJS"],
-        desc:"I have built this site using ReactJS. The aim of building this website is to project my web development skills with an emphasis on ReactJS",
+        desc:"I have built this site using ReactJS. The aim of building this website is to project my web development skills with an emphasis on ReactJS.",
+    },
+    {
+        link: "https://github.com/ronak4195/motoMG",
+        title: "gearPIT",
+        date: "16/02/2024",
+        tech: ["ReactJS", "MongoDB", "ExpressJS", "NodeJS"],
+        desc:"Designed and developed a dynamic eCommerce platform leveraging the MERN (MongoDB, Express.js, React.js, Node.js) stack. Implemented RESTful APIs to enable seamless interaction between the frontend and backend systems. ",
     },
     {
         link: "https://github.com/ronak4195/LC3-VM",
-        title: "JPGMemoRay",
-        date: "09/01/2024",
-        tech: [" C", "Git", "GCC", "Linux"],
+        title: "LC3-VM",
+        date: "19/09/2023",
+        tech: ["C", "ASM", "GCC", "Linux", "Makefile"],
         desc:"A complete simulation of a 16-bit computer system, translating LC3 machine code. Developed LC3 VM, including simulation, memory management, and ISA execution. Achieved seamless machine interpretation by programming 13 Opcode functionalities, 6 Trap routines, and 2 Memory-mapped registers for effective I/O simulation. Implemented an intuitive source code design by isolating CPU and Memory essentials into separate files, while introducing a structured Makefile, for effortless builds. Gained operational and design insights on Assembly, Compilers, and Computer Execution.",
     },
     {
         link: "https://github.com/ronak4195/ProcessPro",
         title: "ProcessPro",
-        date: "19/09/2023",
+        date: "15/04/2023",
         tech: ["Python", "OS", "dd", "grep", "proc", "File System", "Linux"],
         desc:"Python scripts armed to seek kernel-level configurations and manipulate active processes’ data. ◦ Programmed a utility capable of fetching, scanning and writing memory layouts, using proc File System. ◦ Used system tools like dd, grep, and Python’s subprocess library to automate routined OS level tasks, reducing trivial manual interventions by 60%. ◦ Gained practical knowledge accessing and modifying Linux Internals and Kernel Data Structures."
     },
     {
-        link: "https://github.com/ronak4195/LC3-VM",
-        title: "LC3-VM",
-        date: "09/04/2023",
-        tech: ["C", "ASM", "GCC", "Linux", "Makefile"],
+        link: "https://github.com/ronak4195/JPGMemoRay",
+        title: "JPGMemoRay",
+        date: "09/01/2023",
+        tech: [" C", "Git", "GCC", "Linux"],
         desc:"A complete simulation of a 16-bit computer system, translating LC3 machine code. Developed LC3 VM, including simulation, memory management, and ISA execution. Achieved seamless machine interpretation by programming 13 Opcode functionalities, 6 Trap routines, and 2 Memory-mapped registers for effective I/O simulation. Implemented an intuitive source code design by isolating CPU and Memory essentials into separate files, while introducing a structured Makefile, for effortless builds. Gained operational and design insights on Assembly, Compilers, and Computer Execution.",
     },
+    
+    
 ];
 
 export default projectData;
