635965eeb0161a21c6a1067cf0db75d7c00d46bd
ronak4195 <4195rj@gmail.com>
2024-11-09 19:22:09 +0530

    colour

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Clock from "./Clock";
+<<<<<<< HEAD
+import './css/Navbar.css';
+=======
+>>>>>>> 3949d9455c3a5ba74f252b74716249de571e1681
 // import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div>
+<<<<<<< HEAD
+      <nav className="navbar" >
+          <a href="/" className="navbarLogo">
+=======
       <nav className="navbar" style={{display: 'flex', justifyContent: 'space-between', backgroundColor: '#e3f2fd' , height : '8vh'}}>
           <a href="/" className="navbarLogo" style={{textDecoration:"none", color:"black"}}>
+>>>>>>> 3949d9455c3a5ba74f252b74716249de571e1681
             RJ
           </a>
           <Clock/>
--- src/components/RespNavbar.jsx
+++ src/components/RespNavbar.jsx
@@ -7,6 +7,18 @@ export const RespNavbar = () => {
     return (
         <div >
             <nav className="respNavbar" >
+<<<<<<< HEAD
+                <Link to="/" className="link" style={{ color: menu === "HOME" ? "#DCA47D" : "#F7F7F7"}} onClick={() => setMenu("HOME")}>
+                    Home
+                </Link>
+                <Link to="/work"  className="link"  style={{ color: menu === "Work" ? "#DCA47D" : "#F7F7F7"}} onClick={() => setMenu("Work")}>
+                    Work
+                </Link>
+                <Link to="/contact"  className="link"  style={{ color: menu === "Contact" ? "#DCA47D" : "#F7F7F7"}} onClick={() => setMenu("Contact")}>
+                    Contact
+                </Link>
+                <Link to="/blog"  className="link"  style={{ color: menu === "Blog" ? "#DCA47D" : "#F7F7F7"}} onClick={() => setMenu("Blog")}>
+=======
                 <Link to="/" className="link" style={{ color: menu === "HOME" ? "cyan" : "#e3f2fd"}} onClick={() => setMenu("HOME")}>
                     Home
                 </Link>
@@ -17,6 +29,7 @@ export const RespNavbar = () => {
                     Contact
                 </Link>
                 <Link to="/blog"  className="link"  style={{ color: menu === "Blog" ? "cyan" : "#e3f2fd"}} onClick={() => setMenu("Blog")}>
+>>>>>>> 3949d9455c3a5ba74f252b74716249de571e1681
                     Blog
                 </Link>
             </nav>
