4e48108c9966d04a4ced2584399d759e5521d882
ronak4195 <4195rj@gmail.com>
2024-11-09 19:36:38 +0530

    srcupdate

--- src/components/RespNavbar.jsx
+++ src/components/RespNavbar.jsx
@@ -7,7 +7,6 @@ export const RespNavbar = () => {
     return (
         <div >
             <nav className="respNavbar" >
-<<<<<<< HEAD
                 <Link to="/" className="link" style={{ color: menu === "HOME" ? "#DCA47D" : "#F7F7F7"}} onClick={() => setMenu("HOME")}>
                     Home
                 </Link>
@@ -18,18 +17,6 @@ export const RespNavbar = () => {
                     Contact
                 </Link>
                 <Link to="/blog"  className="link"  style={{ color: menu === "Blog" ? "#DCA47D" : "#F7F7F7"}} onClick={() => setMenu("Blog")}>
-=======
-                <Link to="/" className="link" style={{ color: menu === "HOME" ? "cyan" : "#e3f2fd"}} onClick={() => setMenu("HOME")}>
-                    Home
-                </Link>
-                <Link to="/work"  className="link"  style={{ color: menu === "Work" ? "cyan" : "#e3f2fd"}} onClick={() => setMenu("Work")}>
-                    Work
-                </Link>
-                <Link to="/contact"  className="link"  style={{ color: menu === "Contact" ? "cyan" : "#e3f2fd"}} onClick={() => setMenu("Contact")}>
-                    Contact
-                </Link>
-                <Link to="/blog"  className="link"  style={{ color: menu === "Blog" ? "cyan" : "#e3f2fd"}} onClick={() => setMenu("Blog")}>
->>>>>>> 3949d9455c3a5ba74f252b74716249de571e1681
                     Blog
                 </Link>
             </nav>
